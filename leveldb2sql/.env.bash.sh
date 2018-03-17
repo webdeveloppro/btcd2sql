97a31d9e9a7ceea4939ae0b97da8e63c6516bfa6
Vladyslav <vladka@webdevelop.pro>
2018-03-16 18:25:17 -0700

    Added bash env

---
+++ leveldb2sql/.env.bash.sh
@@ -0,0 +1,7 @@
+export GOPATH=/home/vlad/projects/crypto_exchange/bitcoin2sql
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/vlad/projects/crypto_exchange/bin:/home/vlad/projects/crypto_exchange/bitcoin2sql/bin
+export DB_USERNAME=postgres
+export DB_HOST=127.0.0.1
+export DB_PASSWORD=
+export DB_NAME=bitcoin
+export BTCD_DATADIR=/mnt/golang_bitcoin/.btcd/data/mainnet/blocks_ffldb
